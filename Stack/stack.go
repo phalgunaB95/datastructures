6ed6af2403e7dc07c98173acd6e055c041f2c44d
Phalguna Bandi <phalguna.bandi@nyu.edu>
2022-05-30 23:03:45 -0400

    Update stack.go

--- Stack/stack.go
+++ Stack/stack.go
@@ -1,33 +1,48 @@
 package stack
 
+const DefaultSize = 128
+
 type Stack[T any] interface {
 	Push(element T)
-	Pop() (T, bool)
+	Top() T
+	Pop()
 	Len() int
+	IsEmpty() bool
 }
 type stack[T any] struct {
 	elements []T
+	sp       int
+}
+
+func (s *stack[T]) expand() {
+	l := len(s.elements) << 1
+	x := make([]T, l)
+	copy(x, s.elements)
+	s.elements = x
 }
 
 func (s *stack[T]) Push(element T) {
-	s.elements = append(s.elements, element)
-}
-
-func (s *stack[T]) Pop() (T, bool) {
-	var top T
-	l := len(s.elements)
-	if l == 0 {
-		return top, false
+	s.sp++
+	if s.sp == len(s.elements) {
+		s.expand()
 	}
-
-	top = s.elements[l-1]
-	s.elements = s.elements[:l-1]
-	return top, true
+	s.elements[s.sp] = element
 }
-
+func (s *stack[T]) Top() T {
+	return s.elements[s.sp]
+}
+func (s *stack[T]) Pop() {
+	s.sp--
+}
 func (s *stack[T]) Len() int {
-	return len(s.elements)
+	return s.sp
+}
+func (s *stack[T]) IsEmpty() bool {
+	return s.sp == -1
 }
 func NewStack[T any]() *stack[T] {
-	return &stack[T]{make([]T, 0)}
+	return &stack[T]{make([]T, DefaultSize), -1}
+}
+func NewStackN[T any](n int) *stack[T] {
+	return &stack[T]{make([]T, n), -1}
 }
